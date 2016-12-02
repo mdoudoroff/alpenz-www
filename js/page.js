f3693d437edfba551495738e34519dd978969b93
Martin Doudoroff <martin@doudoroff.com>
2016-12-02 11:04:44 -0500

    fixes

--- js/page.js
+++ js/page.js
@@ -2,7 +2,50 @@
 // @codekit-prepend "photoswipe-ui-default.js";
 
 
+//var searchPrompt = 'Search by ingredient name...';
+var searchPrompt = '';
+
 jQuery(document).ready(function() {
+
+	// dynamic search binding (keyup-based)
+	$('#searchBox').keyup(function() {
+		
+		var searchStr = $('#searchBox').val();
+		var searchStrLeadTerm = searchStr.toLowerCase().split(' ')[0];
+		var searchResults = $('#searchResults');
+
+		searchResults.hide();
+
+		if (searchStr.length > 1) {
+			$.getJSON('http://localhost:6543/search/'+searchStr,function(msg) {
+
+				window.console.log(msg);
+
+				searchResults.empty();
+
+				if (msg.length>0) {
+
+					window.console.log('boom!');
+
+					// list results
+					for (var i = 0; i < msg.length; i++) {
+						searchResults.append($('<p><a href="'+msg[i].url+'">'+msg[i].name+'</a><br />'+msg[i].summary+'</p>'));
+					}
+					searchResults.show();
+				}
+				else if (searchStr.length > 0) {
+					searchResults.append($('<p><a href=""><em>No matches. Try searching on the first few letters of a product or category.</em></a></p>'));
+					searchResults.show();
+				} else {
+				}
+			});
+		}
+		else {
+		}
+
+	});
+
+	//
 
 
 
