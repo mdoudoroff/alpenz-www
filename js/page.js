ca527326df4c2636093b613ad403523a097899df
Martin Doudoroff <martin@doudoroff.com>
2016-12-29 13:05:58 -0500

    search work

--- js/page.js
+++ js/page.js
@@ -46,9 +46,9 @@ jQuery(document).ready(function() {
 					for (var i = 0; i < msg.length; i++) {
 
 						if (msg[i].icons) {
-							searchResults.append($('<a href="'+msg[i].url+'"><div class="match"><div class="icons">'+msg[i].icons+'</div><div class="summary"><p><strong>'+msg[i].name+'</strong><br />'+msg[i].summary+'</p></div></div></a>'));
+							searchResults.append($('<a href="'+msg[i].url+'"><div class="match clr"><div class="icons">'+msg[i].icons+'</div><div class="summary"><p><strong>'+msg[i].name+'</strong><br />'+msg[i].summary+'</p></div></div></a>'));
 						} else {
-							searchResults.append($('<a href="'+msg[i].url+'"><div class="match"><div class="summary"><p><strong>'+msg[i].name+'</strong><br />'+msg[i].summary+'</p></div></div></a>'));	
+							searchResults.append($('<a href="'+msg[i].url+'"><div class="match clr"><div class="summary"><p><strong>'+msg[i].name+'</strong><br />'+msg[i].summary+'</p></div></div></a>'));	
 						}
 						
 					}
